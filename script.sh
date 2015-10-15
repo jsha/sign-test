fd03dd77c3fc5e01a106532e8456a62047d8dd03
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-10-14 22:35:25 -0700

    More updates.

--- script.sh
+++ script.sh
@@ -4,8 +4,13 @@ openssl req -x509 -new -newkey rsa:2048 -out root.pem -keyout root-key.pem -days
 
 openssl x509 -text -noout -in root.pem 
 
-openssl req -new -newkey rsa:2048 -out cross.csr -keyout key.pem -days 1825  -nodes -subj "/CN=cyber cyber cyber fake CA" -extensions v3_ca -config cross.cnf
+openssl req -new -newkey rsa:2048 -out cross.csr -keyout cross-key.pem -days 1825  -nodes -subj "/CN=cyber cyber cyber fake CA" -extensions v3_ca -config cross.cnf
 
-openssl ca -in cross.csr -out cross.pem -days 1825 -notext -md sha256  -extensions v3_ca -config cross.cnf -name default_ca -batch
+openssl ca -in cross.csr -out cross.pem -days 1825 -notext -md sha256  -extensions v3_ca -config cross.cnf -name default_ca -batch -subj "/CN=cyber intermediate"
+
+openssl req -new -newkey rsa:2048 -out ee.csr -keyout ee-key.pem -days 1825  -nodes -extensions v3_ca -config ee.cnf -subj "/CN=localhost"
+openssl ca -in ee.csr -out ee.pem -days 365 -notext -md sha256  -extensions v3_ca -config ee.cnf -name default_ca -batch
+
+openssl s_server -accept 7443 -www -key ee-key.pem -cert ee.pem
 
 #openssl x509 -text -noout -in cert.pem
