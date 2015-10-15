305558fb7cde8932fa737e5292e4ff855548d966
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-10-14 20:49:12 -0700

    Add root.

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -o errexit
-openssl req -x509 -new -newkey rsa:2048 -out cert.pem -keyout key.pem -days 1825  -nodes -subj "/CN=cyber cyber cyber fake CA" -extensions v3_ca -config cross.cnf
+openssl req -x509 -new -newkey rsa:2048 -out root.pem -keyout root-key.pem -days 1825  -nodes -subj "/CN=cyber cyber cyber fake CA" -extensions v3_ca -config root.cnf
 
-openssl x509 -text -noout -in cert.pem
+openssl x509 -text -noout -in root.pem 
+
+openssl req -new -newkey rsa:2048 -out cross.csr -keyout key.pem -days 1825  -nodes -subj "/CN=cyber cyber cyber fake CA" -extensions v3_ca -config cross.cnf
+
+openssl ca -in cross.csr -out cross.pem -days 1825 -notext -md sha256  -extensions v3_ca -config cross.cnf -name default_ca -batch
+
+#openssl x509 -text -noout -in cert.pem
