5f061de0b8c7d93ec043bfce25004302e582a261
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-01-05 13:00:09 -0800

    Make names say "happy hacker."

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -o errexit
-openssl req -x509 -new -newkey rsa:2048 -out root.pem -keyout root-key.pem -days 3650 -nodes -subj "/CN=cheerful charlie's fake ROOT" -extensions v3_ca -config root.cnf
+openssl req -x509 -new -newkey rsa:2048 -out root.pem -keyout root-key.pem -days 3650 -nodes -subj "/CN=happy hacker fake ROOT 2" -extensions v3_ca -config root.cnf
 
 openssl x509 -text -noout -in root.pem
 
-openssl req -new -newkey rsa:2048 -out cross.csr -keyout cross-key.pem -days 1825  -nodes -subj "/CN=honest henry's fake INTERMEDIATE" -extensions v3_ca -config cross.cnf
+openssl req -new -newkey rsa:2048 -out cross.csr -keyout cross-key.pem -days 1825  -nodes -subj "/CN=happy hacker fake INTERMEDIATE 2" -extensions v3_ca -config cross.cnf
 
 openssl ca -in cross.csr -out cross.pem -days 1825 -notext -md sha256  -extensions v3_ca -config cross.cnf -name default_ca -batch
 openssl x509 -text -noout -in cross.pem
