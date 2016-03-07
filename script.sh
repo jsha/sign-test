ffdbc49ea15e8b2c87e7f08df779d745048dd28b
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-03-07 09:26:18 -0800

    rm database before script

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -o errexit
+rm database.txt
+
 openssl req -x509 -new -newkey rsa:2048 -out root.pem -keyout root-key.pem -days 1825  -nodes -subj "/CN=cyber cyber cyber fake CA" -extensions v3_ca -config root.cnf
 
 openssl x509 -text -noout -in root.pem 
