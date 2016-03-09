101b9ad7c12cc8f76d192b73f4bff9bc881afbbe
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-03-08 18:26:00 -0800

    Improve script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ set -o xtrace
 
 # Truncate the database. Without this, we get a segfault, not sure why.
 > database.txt
+> ee-database.txt
 
 openssl req -x509 -new -newkey rsa:2048 -out root.pem -keyout root-key.pem -days 1825  -nodes -subj "/CN=cyber cyber cyber fake CA" -extensions v3_ca -config root.cnf
 
@@ -15,9 +16,11 @@ openssl ca -in cross.csr -out cross.pem -days 1825 -notext -md sha256 -extension
 
 openssl x509 -text -noout -in cross.pem
 
-openssl req -new -newkey rsa:2048 -out ee.csr -keyout ee-key.pem -days 1825  -nodes -extensions v3_ca -config ee.cnf -subj "/CN=localhost"
+openssl req -new -newkey rsa:2048 -out ee.csr -keyout ee-key.pem -days 1825 -nodes -extensions v3_ca -config ee.cnf -subj "/CN=localhost"
 openssl ca -in ee.csr -out ee.pem -days 365 -notext -md sha256  -extensions v3_ca -config ee.cnf -name default_ca -batch
 
-openssl s_server -accept 7443 -www -key ee-key.pem -cert ee.pem
+openssl x509 -text -noout -in ee.pem
 
-#openssl x509 -text -noout -in cert.pem
+cat ee.pem cross.pem > fullchain.pem
+
+#openssl s_server -accept 7443 -www -key ee-key.pem -cert ee.pem </dev/null
